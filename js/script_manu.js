1e9af6505ff8cb796977a8f43827430860572c1d
Maiminase93 <196683919+Maiminase93@users.noreply.github.com>
2025-02-17 20:05:27 +0100

    second commit

---
+++ js/script_manu.js
@@ -0,0 +1,18 @@
+let boton = document.querySelector('#boton-cambio');
+let video = document.querySelector('#video1');
+const arrayVideos = ['videos/video1.mp4', 'videos/video2.mp4', 'videos/video3.mp4'];
+
+boton.addEventListener('click', ()=>{
+    let src = video.getAttribute('src');
+    switch(src){
+        case 'videos/video1.mp4':
+        video.setAttribute('src', arrayVideos[1]);
+        break;
+        case 'videos/video2.mp4':
+            video.setAttribute('src', arrayVideos[2]);
+            break;
+        case 'videos/video3.mp4':
+        video.setAttribute('src', arrayVideos[0]);
+        break;
+    }
+})
